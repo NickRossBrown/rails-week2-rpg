65321c867d035846c23fb9702249dea4555c25a4
Nick Brown and Rita Bennett-Chew <nick@>
2018-09-13 16:04:02 -0700

    Ensure admin route is inaccessible when typing in URL

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,8 @@
 class AdminController < ApplicationController
+  before_action :is_admin?
 
   def show
-    # binding.pry
+    binding.pry
   end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
   helper_method :is_admin?
-  
+
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
@@ -10,7 +10,12 @@ class ApplicationController < ActionController::Base
   end
 
   def is_admin?
-    current_user && current_user.admin
+    if current_user && current_user.admin
+      return true
+    else
+      flash[:alert] = "You aren't authorized to visit that page"
+      redirect_to '/signin'
+    end
   end
 
 end
